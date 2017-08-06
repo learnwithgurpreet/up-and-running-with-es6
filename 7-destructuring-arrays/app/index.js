456787c85309a300622b7bf5eb05f758e14d02d5
Gurpreet Singh 4 <gsingh9@sapient.com>
2017-08-07 00:58:50 +0530

    destructring of arrays

---
+++ 7-destructuring-arrays/app/index.js
@@ -0,0 +1,11 @@
+function run(...param) {
+  const fellowship = param;
+  let [var1, var2, var3, var4, var5] = fellowship;
+  console.log(var1);  //Lorem
+  console.log(var2);  //ipsum
+  console.log(var3);  //dolor
+  console.log(var4);  //sit
+  console.log(var5);  //amet
+};
+
+run('Lorem', 'ipsum', 'dolor', 'sit', 'amet');
